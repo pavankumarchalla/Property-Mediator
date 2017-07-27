13b78af61883be2aeaceded25e7def751b6c02d8
Raja Jawahar <rajajawahar77@gmail.com>
2017-07-28 00:32:06 +0530

    Add Client Form Validation completed

--- Property Mediator/ClientFormViewController.swift
+++ Property Mediator/ClientFormViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ClientFormViewController: UIViewController {
 
+    @IBOutlet weak var notesInput: UITextView!
+    @IBOutlet weak var emailAddress: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var panNumber: UITextField!
+    @IBOutlet weak var nameInputBox: UITextField!
+    
+    @IBAction func saveAction(_ sender: Any) {
+        if (nameInputBox.text?.isEmpty)! {
+            showAlertView(alert: "Please Enter Name")
+            return
+        }
+        if (panNumber.text?.isEmpty)! {
+            showAlertView(alert: "Please PAN Number")
+            return
+        }
+        if (phoneNumber.text?.isEmpty)! {
+            showAlertView(alert: "Please Enter Phone Number")
+            return
+        }
+        if (emailAddress.text?.isEmpty)! {
+            showAlertView(alert: "Please Enter Email Address")
+            return
+        }
+      
+        if (notesInput.text?.isEmpty)! {
+            showAlertView(alert: "Please Enter Notes")
+            return
+        }
+        
+        
+    }
+    @IBAction func cancelAction(_ sender: Any) {
+        _ = navigationController?.popViewController(animated: true)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +52,13 @@ class ClientFormViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func showAlertView(alert:String) {
+        let alertView = UIAlertController(title: "Validation Error", message: alert, preferredStyle: .alert)
+        alertView.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alertView, animated: true, completion: nil)
+        
     }
     
 
