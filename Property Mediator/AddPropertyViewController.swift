dedc69c410e6e80807cb1f618e9a10733c9014d4
Raja Jawahar <rajajawahar77@gmail.com>
2017-07-30 23:02:58 +0530

    Gallery Management added for Add Property

--- Property Mediator/AddPropertyViewController.swift
+++ Property Mediator/AddPropertyViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class AddPropertyViewController: UIViewController {
+class AddPropertyViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var galleryBtn: UIButton!
+    
+    var picker:UIImagePickerController?=UIImagePickerController()
 
     @IBAction func cancel(_ sender: Any) {
         _ = navigationController?.popViewController(animated: true)
@@ -17,10 +21,42 @@ class AddPropertyViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker?.delegate=self
+        galleryBtn.addTarget(self, action:#selector(self.openGallery), for: .touchUpInside)
+
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func openGallery()
+    {
+        picker!.allowsEditing = false
+        picker!.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        
+        picker?.allowsEditing = false
+        picker?.sourceType = .photoLibrary
+        picker?.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        picker?.modalPresentationStyle = .popover
+        present(picker!, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+        } else{
+            print("Something went wrong")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
 }
