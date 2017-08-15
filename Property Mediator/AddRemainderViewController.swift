86e383e4a28d202938a5583bc364a9d234221db3
Raja Jawahar <rajajawahar77@gmail.com>
2017-08-15 22:50:48 +0530

    Add Remainder View Controller added

---
+++ Property Mediator/AddRemainderViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddRemainderViewController.swift
+//  Property Mediator
+//
+//  Created by Raja Mohamed on 15/08/17.
+//  Copyright © 2017 sedintechnologies. All rights reserved.
+//
+
+import UIKit
+
+class AddRemainderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
