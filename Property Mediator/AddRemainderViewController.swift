e9d5b5c6cc5eb71e23a763f99835f1bb4cac6545
Raja Jawahar <rajajawahar77@gmail.com>
2017-08-15 23:49:28 +0530

    Add RemainderViewController added

--- Property Mediator/AddRemainderViewController.swift
+++ Property Mediator/AddRemainderViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import EventKit
 
 class AddRemainderViewController: UIViewController {
 
+    @IBOutlet weak var dueDateTime: UITextField!
+    @IBOutlet weak var remainderDescription: UITextView!
+    
+    @IBAction func cancel(_ sender: Any) {
+        _ = navigationController?.popViewController(animated: true)
+
+    }
+    var eventStore = EKEventStore()
+    var datePicker: UIDatePicker!
+    
+    
+    @IBAction func saveRemainder(_ sender: Any) {
+        let reminder = EKReminder(eventStore: self.eventStore)
+        reminder.title = remainderDescription.text
+        reminder.calendar = eventStore.defaultCalendarForNewReminders()
+        let timezoneLK = NSTimeZone(forSecondsFromGMT: 19800)
+        let components = NSCalendar.current.dateComponents(in: timezoneLK as TimeZone, from: self.datePicker.date)
+        reminder.dueDateComponents = components
+        do {
+            try eventStore.save(reminder,
+                                commit: true)
+        } catch let error {
+            print("Reminder failed with error \(error.localizedDescription)")
+        }
+        _ = navigationController?.popViewController(animated: true)
+
+      
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        datePicker = UIDatePicker()
+        datePicker.addTarget(self, action: #selector(AddRemainderViewController.addDate), for: UIControlEvents.valueChanged)
+        
+        datePicker.datePickerMode = UIDatePickerMode.dateAndTime
+        dueDateTime.inputView = datePicker
+        remainderDescription.becomeFirstResponder()
+        
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    func addDate(){
+        self.dueDateTime.text = self.datePicker.date.description
+    }
 
    
 }
