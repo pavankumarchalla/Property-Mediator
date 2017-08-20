08ba22775f84ccc32de4d819bd148f8fa01b802e
Raja Jawahar <rajajawahar77@gmail.com>
2017-08-20 21:04:53 +0530

    CategoryViewController added

---
+++ Property Mediator/CategoryViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryViewController.swift
+//  Property Mediator
+//
+//  Created by Raja Mohamed on 20/08/17.
+//  Copyright © 2017 sedintechnologies. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
